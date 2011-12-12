a4247ea651522df45aee3c7f18a13e4550ba8c22
Stefan Hornburg (Racke) <racke@linuxia.de>
2011-12-12 11:23:28 +0100

    Protect development environments with HTTP logins from password file.

--- bin/app.pl
+++ bin/app.pl
@@ -1,4 +1,24 @@
 #!/usr/bin/env perl
+
 use Dancer;
 use Flowers;
-dance;
+use Plack::Builder;
+
+my $app = sub {
+    my $env = shift;
+    my $request = Dancer::Request->new(env => $env);
+    Dancer->dance($request);
+};
+
+builder {
+    my $dancer_env;
+    
+    Dancer::Config::load();
+    $dancer_env = Dancer::Config::setting('environment');
+    
+    enable_if {$env ne 'production'} "Auth::Htpasswd", file => 'run/.htpasswd';
+    
+    $app;
+};
+
+
