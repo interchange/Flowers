c419846ffa6506cbe3e96bdaa8abbb53d9778bc8
Marco Pessotto <melmothx@gmail.com>
2013-03-14 16:03:15 +0100

    Made room for the new payment mechanism

--- lib/Flowers/Routes/Checkout.pm
+++ lib/Flowers/Routes/Checkout.pm
@@ -8,7 +8,7 @@ use DateTime;
 use DateTime::Duration;
 use DateTime::Locale;
 use Input::Validator;
-use Business::OnlinePayment;
+use Business::OnlinePayment::IPayment;
 
 use Flowers::Address;
 use Flowers::Transactions;
@@ -25,18 +25,10 @@ get '/checkout' => sub {
 post '/checkout' => sub {
     my ($form, $values, $validator, $error_ref, $form_last);
 
-    $form_last = 'giftinfo';
+    $form = form('giftinfo');
     
-    for my $name (qw/giftinfo payment/) {
-	$form = form($name);
+    # sorry for the indentation off, but I want a clean patch
 
-	unless ($form->valid) {
-	    $form_last = $name;
-	    last;
-	}
-    }
-    
-    if ($form_last eq 'giftinfo') {
 	$values = $form->values;
 
 	# validate form input
@@ -69,130 +61,15 @@ post '/checkout' => sub {
 	    $form->valid(1);
 
 	    $form = form('payment');
+            # here we have to do the soap request creating the session
+            # then we intercept the client on another route and
+            # display checkout-thanks, destroying the session, maybe,
+            # or at least clearing the cart
+            debug cart->total;
 	    template 'checkout-payment', checkout_tokens($form);
 	}
-    }
-    elsif ($form_last eq 'payment') {
-	# second step
-	$values = $form->values();
-	
-	# validate form input
-	$validator = new Input::Validator;
-
-	$validator->field('name')->required(1);
-	$validator->field('street_address')->required(1);
-	$validator->field('zip')->required(1);
-	$validator->field('city')->required(1);
-	
-	$validator->validate($values);
-
-	if ($validator->has_errors) {
-	    $error_ref = $validator->errors;
-	    $error_ref->{zip_city} = $error_ref->{zip} || $error_ref->{city};
-
-	    if (exists $error_ref->{zip}) {
-		$error_ref->{zip_city} = $error_ref->{zip};
-	    }
-	    
-	    debug("Errors: ", $error_ref);
-
-	
-	
-	    # back to second step
-	    $form->fill($values);
-	    template 'checkout-payment', checkout_tokens($form, $error_ref);
-	}
-	else {
-	    # charge amount
-	    my ($expiration, $tx);
-
-	    $expiration = sprintf("%02d%02d", $values->{cc_month}, $values->{cc_year});
-	    
-	    $tx = charge(amount => cart->total,
-		card_number => $values->{cc_number},
-		expiration => $expiration,
-		cvc => $values->{cvc_number});
-
-	    if ($tx->is_success()) {
-		if ($tx->can('popup_url')) {
-		    debug("Payment redirect: ", $tx->popup_url());
-		
-		    return redirect $tx->popup_url();
-		}
-
-		debug "Payment successful: ", $tx->authorization;
-
-		my ($addr_form, $addr_values, $addr_ship, $addr_bill, $tx_order);
-		
-		# create delivery address from gift info form
-		debug("Looking at giftinfo form.");
-		$addr_form = form('giftinfo');
-		$addr_values = $addr_form->values('session');
-		debug("Delivery address values: ", $addr_values);
-
-		$addr_ship = Flowers::Address->new(%$addr_values);
-		$addr_ship->save;
-
-		# create billing address from payment form
-		
-		# create transaction
-		$tx_order = Flowers::Transactions->new();
-		$tx_order->submit;
-		
-		template 'checkout-thanks', checkout_tokens($form);
-	    }
-	    else {
-		$error_ref->{payment_error} = $tx->error_message;
-		template 'checkout-payment', checkout_tokens($form, $error_ref);
-	    }
-	}
-    }
 };
 
-sub charge {
-    my (%args) = @_;
-    my ($tx, $settings);
-
-    $settings = config->{plugins}->{Nitesi}->{Payment}->{providers}->{config->{payment_method}};
-    
-    $tx = Business::OnlinePayment->new(config->{payment_method}, %$settings);
-
-    if ($settings->{server}) {
-	$tx->server($settings->{server});
-    }
-    
-    # Sofortbanking expects amount as xx.xx
-    $args{amount} = sprintf('%.2f', $args{amount});
-
-    $tx->content(%$settings,
-		 amount => $args{amount},
-		 card_number => $args{card_number},
-		 expiration => $args{expiration},
-		 cvc => $args{cvc},
-		 login => $settings->{login},
-		 password => $settings->{password},
-		 type => $args{type} || $settings->{type} || 'CC',
-		 action => $args{action} || $settings->{action} || 'Authorization Only',
-	);
-
-    eval {
-	$tx->submit();
-    };
-
-    if ($@) {
-	die "Payment ", config->{payment_method}, " failed: ", $@;
-    }
-    
-    if ($tx->is_success()) {
-	if ($tx->can('popup_url')) {
-	    debug("Success!  Redirect browser to ". $tx->popup_url());
-	}
-    } else {
-        debug("Card was rejected: " . $tx->error_message);
-    }
-
-    return $tx;
-}
 
 sub checkout_tokens {
     my ($form, $errors, $tokens) = @_;
