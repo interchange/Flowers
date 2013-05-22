7333a441aeebf50be33009c11829fb10c710d4b4
Stefan Hornburg (Racke) <racke@linuxia.de>
2013-05-22 11:04:23 +0200

    Remove Plack::Builder from bin/app.pl.

--- bin/app.pl
+++ bin/app.pl
@@ -2,21 +2,5 @@
 
 use Dancer;
 use Flowers;
-use Plack::Builder;
 
-my $app = sub {
-    my $env = shift;
-    my $request = Dancer::Request->new(env => $env);
-    Dancer->dance($request);
-};
-
-builder {
-    my $dancer_env;
-    
-    Dancer::Config::load();
-    $dancer_env = Dancer::Config::setting('environment');
-    
-    $app;
-};
-
-
+dance;
