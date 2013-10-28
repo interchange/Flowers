687af33a0aa3945c10328bb881dc11d6ee40b0f9
Stefan Hornburg (Racke) <racke@linuxia.de>
2013-10-28 20:27:19 +0100

    Use charge function from Dancer::Plugin::Nitesi and a few more changes for checkout.

--- lib/Flowers/Routes/Checkout.pm
+++ lib/Flowers/Routes/Checkout.pm
@@ -8,10 +8,18 @@ use DateTime;
 use DateTime::Duration;
 use DateTime::Locale;
 use Input::Validator;
-use Business::OnlinePayment;
 
 use Flowers::Address;
 use Flowers::Transactions;
+
+hook 'before_cart_display' => sub {
+    my $tokens = shift;
+    my $form = form('giftinfo');
+
+    $form->valid(0);
+
+    template 'checkout-giftinfo', checkout_tokens($form, {}, $tokens);
+};
 
 get '/checkout' => sub {
     my $form;
@@ -83,7 +91,11 @@ post '/checkout' => sub {
 	$validator->field('street_address')->required(1);
 	$validator->field('zip')->required(1);
 	$validator->field('city')->required(1);
-	
+
+    # credit card stuff
+    $validator->field('cc_number')->required(1);
+    $validator->field('cvc_number')->required(1);
+    
 	$validator->validate($values);
 
 	if ($validator->has_errors) {
@@ -93,11 +105,9 @@ post '/checkout' => sub {
 	    if (exists $error_ref->{zip}) {
 		$error_ref->{zip_city} = $error_ref->{zip};
 	    }
-	    
+
 	    debug("Errors: ", $error_ref);
 
-	
-	
 	    # back to second step
 	    $form->fill($values);
 	    template 'checkout-payment', checkout_tokens($form, $error_ref);
@@ -107,92 +117,56 @@ post '/checkout' => sub {
 	    my ($expiration, $tx);
 
 	    $expiration = sprintf("%02d%02d", $values->{cc_month}, $values->{cc_year});
-	    
-	    $tx = charge(amount => cart->total,
-		card_number => $values->{cc_number},
-		expiration => $expiration,
-		cvc => $values->{cvc_number});
+	    my %payment_data = (amount => cart->total,
+                     first_name => 'Test',
+                     last_name => 'Tester',
+                     card_number => $values->{cc_number},
+                     expiration => $expiration,
+                     cvc => $values->{cvc_number});
 
-	    if ($tx->is_success()) {
-		if ($tx->can('popup_url')) {
-		    debug("Payment redirect: ", $tx->popup_url());
+        debug("Payment_data: ", \%payment_data);
+        
+	    $tx = charge(%payment_data);
+
+        if ($tx->is_success()) {
+            if ($tx->can('popup_url')) {
+                debug("Payment redirect: ", $tx->popup_url());
 		
-		    return redirect $tx->popup_url();
-		}
+                return redirect $tx->popup_url();
+            }
 
-		debug "Payment successful: ", $tx->authorization;
+            debug "Payment successful: ", $tx->authorization;
 
-		my ($addr_form, $addr_values, $addr_ship, $addr_bill, $tx_order);
+            my ($addr_form, $addr_values, $addr_ship, $addr_bill, $tx_order);
 		
-		# create delivery address from gift info form
-		debug("Looking at giftinfo form.");
-		$addr_form = form('giftinfo');
-		$addr_values = $addr_form->values('session');
-		debug("Delivery address values: ", $addr_values);
+            # create delivery address from gift info form
+            debug("Looking at giftinfo form.");
+            $addr_form = form('giftinfo');
+            $addr_values = $addr_form->values('session');
+            $addr_values->{country_code} = 'SI';
+            $addr_values->{type} = 'shipping';
+            debug("Delivery address values: ", $addr_values);
 
-		$addr_ship = Flowers::Address->new(%$addr_values);
-		$addr_ship->save;
+            $addr_ship = Flowers::Address->new(%$addr_values);
+            $addr_ship->save;
 
-		# create billing address from payment form
+            # create billing address from payment form
 		
-		# create transaction
-		$tx_order = Flowers::Transactions->new();
-		$tx_order->submit;
-		
-		template 'checkout-thanks', checkout_tokens($form);
-	    }
+            # create transaction
+            $tx_order = Flowers::Transactions->new();
+            $tx_order->submit;
+
+            cart->clear;
+
+            template 'checkout-thanks', checkout_tokens($form);
+        }
 	    else {
-		$error_ref->{payment_error} = $tx->error_message;
-		template 'checkout-payment', checkout_tokens($form, $error_ref);
+            $error_ref->{payment_error} = $tx->error_message;
+            template 'checkout-payment', checkout_tokens($form, $error_ref);
 	    }
 	}
-    }
-};
-
-sub charge {
-    my (%args) = @_;
-    my ($tx, $settings);
-
-    $settings = config->{plugins}->{Nitesi}->{Payment}->{providers}->{config->{payment_method}};
-    
-    $tx = Business::OnlinePayment->new(config->{payment_method}, %$settings);
-
-    if ($settings->{server}) {
-	$tx->server($settings->{server});
-    }
-    
-    # Sofortbanking expects amount as xx.xx
-    $args{amount} = sprintf('%.2f', $args{amount});
-
-    $tx->content(%$settings,
-		 amount => $args{amount},
-		 card_number => $args{card_number},
-		 expiration => $args{expiration},
-		 cvc => $args{cvc},
-		 login => $settings->{login},
-		 password => $settings->{password},
-		 type => $args{type} || $settings->{type} || 'CC',
-		 action => $args{action} || $settings->{action} || 'Authorization Only',
-	);
-
-    eval {
-	$tx->submit();
-    };
-
-    if ($@) {
-	die "Payment ", config->{payment_method}, " failed: ", $@;
-    }
-    
-    if ($tx->is_success()) {
-	if ($tx->can('popup_url')) {
-	    debug("Success!  Redirect browser to ". $tx->popup_url());
-	}
-    } else {
-        debug("Card was rejected: " . $tx->error_message);
-    }
-
-    return $tx;
 }
+};
 
 sub checkout_tokens {
     my ($form, $errors, $tokens) = @_;
@@ -218,20 +192,18 @@ sub checkout_tokens {
     for my $name (@{$dtl->month_stand_alone_abbreviated}) {
 	push (@months, {value => $i++, label => ucfirst($name)});
     }
-    debug("Months: ", \@months);
 
     for my $year (2012 .. 2020) {
 	push (@years, {value => substr($year,2,2), label => $year});
     }
-    
-    %$tokens = (form => $form,
-	       layout_noleft => 1,
-	       layout_cartright => 1,
-	       items => cart->items,
-	       days => \@gift_days,
-	       months => \@months,
-	       years => \@years,
-    );
+
+    $tokens->{form} = $form;
+    $tokens->{layout_noleft} = 1;
+    $tokens->{layout_noright} = 1;
+    $tokens->{items} = cart->items;
+    $tokens->{days} = \@gift_days;
+    $tokens->{months} = \@months;
+    $tokens->{years} = \@years;
 
     if ($errors) {
         $tokens->{errors} = $errors;
